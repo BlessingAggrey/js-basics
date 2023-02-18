e9bf6c688cd48ee1f54c793d4376dc2f13197e96
BlessingAggrey <dewigsupreme@outlook.com>
2023-02-18 16:29:04 -0400

    All done

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
 function add(value1, value2) {}
 function add( value1, value2) {return value1 *  value2}
+console.log(add(20,10));
+
+let myFirstname = "Blessing";
+let myLastname = "DeNwigwe-Aggrey";
+let myStuterntrack = "Cohort-1.4/Frontend";
+let mySkills = "HTML, CSS, JavaScript";
+let myFavouriteColour = "Pink";
+let myCountryIndependence = 1960;
+
+console.log("My name is " + myFirstname + " " + myLastname + ", " + "I'm currently enrolled in Stutern " + myStuterntrack + " and my skills so far are; " + mySkills + ", " + "my favourite colour is " + myFavouriteColour + "and my country gained independence in " + myCountryIndependence + ".");
