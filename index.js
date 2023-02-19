3b97365d68fe694910b37747ddd1683bbbad6305
BlessingAggrey <dewigsupreme@outlook.com>
2023-02-19 10:12:43 -0400

    All done

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
+// let result = document.getElementById('result')
+
+// result.innerText =  'Result here...'
+
+
 function add(value1, value2) {}
 function add( value1, value2) {return value1 *  value2}
 console.log(add(20,10));
